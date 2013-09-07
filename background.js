6f50537470b60e16e614eee363258990f8b7a7fa
david <david_wan@rocketmail.com>
2013-09-07 11:11:31 -0400

    Fixed requests

--- background.js
+++ background.js
@@ -3,6 +3,7 @@ function onRequest(request, sender, callback) {
   if (request.action == 'getJSON') {
   	console.log("successfully received");
     $.getJSON(request.url, callback);
+    console.log(request.url);
   }
 }
 
--- home.js
+++ home.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	console.log("working");
     $("#login_button").click( function (e) {
     	console.log($("#bitcoin_addr").val());
-    	var callUrl = 'http://ec2-23-22-205-148.compute-1.amazonaws.com:8000/login/' + $("#bitcoin_addr").val();
+    	var callUrl = 'http://ec2-23-22-205-148.compute-1.amazonaws.com:8000/login?bitcoin_addr=' + $("#bitcoin_addr").val();
         chrome.extension.sendRequest({action:'getJSON',url:callUrl}, function () {
         	chrome.storage.sync.set({'bitcoin_addr': $("#bitcoin_addr").val(),
                                      'current_balance': data['current_balance']}, function () {
