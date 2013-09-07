a8e4d2d445fd8a5ff8c44ce65fb8100141904acf
david <david_wan@rocketmail.com>
2013-09-07 19:48:24 -0400

    updated frontend

--- background.js
+++ background.js
@@ -1,10 +1,31 @@
 console.log("Something happened.");
-function onRequest(request, sender, callback) {
+function onMessage(request, sender, sendResponse) {
   if (request.action == 'getJSON') {
-  	console.log("successfully received");
-    $.getJSON(request.url, callback);
-    console.log(request.url);
-  }
-}
+  	$.ajax({url:request.url, 
+  		dataType: 'json',
+  		success:function(data) {
+  			sendResponse(data);
+	}});
+	
+	return true;
+}};
 
-chrome.extension.onRequest.addListener(onRequest);
+chrome.runtime.onMessage.addListener(onMessage);
+
+chrome.extension.onRequest.addListener(function(resquest, sender, sendResponse) {
+        if(request.method == "send-payment") {
+                chrome.storage.local.get("bitcoin_addr", function(data) {
+                        var callUrl = 'http://ec2-23-22-205-148.compute-1.amazonaws.com:8000/send-payment?bitcoin_addr=' + data + '&website=' + request.url;
+                        chrome.extension.sendRequest({action:'getJSON',url:callUrl}, function (data) {
+                                var total_earned = data['total_earned'];
+                                alert("YOUR TOTAL IS : "+ total_earned);
+                                chrome.storage.local.set({"total_earned": total_earned}, function(response) {});
+                                chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+                                        chrome.tabs.sendMessage(tabs[0].id, {"total_earned": total_earned}, function(response) {
+                                                console.log(response);
+                                        });
+                                });
+                        });
+            });
+        }
+});
---
+++ detectactions.js
@@ -0,0 +1,28 @@
+var click_count = 0;
+var bitsurf_value = 0.0001;
+var current_earnings = 0;
+var facebook_value =
+ 
+$(document).ready(function() {
+    $('body').on('click', '*', function (e) {
+        click_count++;
+    });
+    window.setInterval(function() {
+        if(click_count >= 3) {
+            chrome.extension.sendRequest({method: "send-payment", url: window.location.hostname}, function (response) {
+                console.log(response.status);
+            });
+            click_count = 0;
+        }
+    }, 30000);
+});
+ 
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+ 
+    console.log(sender.tab ?
+                "from a content script:" + sender.tab.url :
+                "from the extension");
+    if (request.greeting == "hello")
+      sendResponse({farewell: "goodbye"});
+});
