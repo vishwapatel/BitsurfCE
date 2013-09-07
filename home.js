812e05148dbc17c464cfb564ac59adf2b3fb600b
Vishwa Patel <vishwa@seas.upenn.edu>
2013-09-07 11:04:27 -0400

    Nonsense commit

--- home.js
+++ home.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	$(".alert").alert('close​');
 	$("#loginbutton").click( function (e) {
-		$(.getJSON('/login/' + $("#bitcoin_addr").val(), function (data) {
+		$.getJSON('ec2-23-22-205-148.comput​e-1.amazonaws.com:8000/login', {'bitcoin_addr': $("#bitcoin_addr").val()}, function (data) {
 			chrome.storage.sync.set({'bitcoin_addr': $("#bitcoin_addr").val(),
 									 'current_balance': data['current_balance']}, function () {
 									 	window.location.replace("status.html");
