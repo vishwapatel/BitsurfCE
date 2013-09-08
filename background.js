52390197372443d3f659c70c48388764104207e2
david <david_wan@rocketmail.com>
2013-09-07 22:42:43 -0400

    Updated notifications

--- background.js
+++ background.js
@@ -16,18 +16,45 @@ function onMessage(request, sender, sendResponse) {
 	if(request.method == "send-payment") {
 				console.log("hey");
                 chrome.storage.local.get("bitcoin_addr", function(data) {
-                        var callUrl = 'http://ec2-23-22-205-148.compute-1.amazonaws.com:8000/send-payment?bitcoin_addr=' + data['bitcoin_addr'] + '&website=' + request.url;
-                        console.log(callUrl);
-                        onMessage({action:'getJSON',url:callUrl}, null, function (data) {
-                                var total_earned = data['total_earned'];
-                                alert("YOUR TOTAL IS : "+ total_earned);
-                                chrome.storage.local.set({"total_earned": total_earned}, function(response) {});
-                                /*chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-                                        chrome.tabs.sendMessage(tabs[0].id, {"total_earned": total_earned}, function(response) {
-                                                console.log(response);
-                                        });
-                                });*/
-                        });
+                		if (!($.isEmptyObject(data))) {
+	                        var callUrl = 'http://ec2-23-22-205-148.compute-1.amazonaws.com:8000/send-payment?bitcoin_addr=' + data['bitcoin_addr'] + '&website=' + request.url;
+	                        console.log(callUrl);
+	                        onMessage({action:'getJSON',url:callUrl}, null, function (data2) {
+	                                var total_earned = data2['total_earned'];
+	                                chrome.storage.local.set({"total_earned": total_earned}, function(response) {});
+
+	                                //chrome.storage.local.get({"total_earned": total_earned}, function(response) {});
+
+	                                var notifCount;
+						        	chrome.storage.local.get('notifCount', function (data3) {
+						        		if (!($.isEmptyObject(data3))) {
+											notifCount = data3['notifCount'];
+										}
+										else {
+											console.log("else");
+											notifCount = 0;
+										}
+										chrome.storage.local.set({'notifCount': (notifCount + 1)}, function(response) {});
+										console.log(notifCount);
+						        	});
+						        	var newId = 'id' + notifCount; 
+									chrome.notifications.create(
+									  newId,{   
+									      type: 'basic', 
+									      iconUrl: 'toolbar_icon.png', 
+									      title: 'bitSurf', 
+									      message: 'You now have ' + data2['total_earned'] + ' BTC',
+									      priority: 0},
+									  function() { /* Error checking goes here */} 
+
+									);
+	                                /*chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+	                                        chrome.tabs.sendMessage(tabs[0].id, {"total_earned": total_earned}, function(response) {
+	                                                console.log(response);
+	                                        });
+	                                });*/
+	                        });
+					}
             });
         }
 	
--- home.js
+++ home.js
@@ -12,6 +12,29 @@ $(document).ready(function() {
     	var callUrl = 'http://ec2-23-22-205-148.compute-1.amazonaws.com:8000/login?bitcoin_addr=' + $("#bitcoin_addr").text("#bitcoin_addr").val();
 
         chrome.runtime.sendMessage({action:'getJSON',url:callUrl}, function(data) {
+
+        	var notifCount;
+        	chrome.storage.local.get('notifCount', function (data) {
+        		if (!($.isEmptyObject(data))) {
+					notifCount = data['notifCount'];
+				}
+				else {
+					notifCount = 0;
+				}
+				chrome.storage.local.set({"notifCount": (notifCount + 1)}, function(response) {});
+        	});
+
+        	chrome.notifications.create(
+								  'id' + notifCount,{   
+								      type: 'basic', 
+								      iconUrl: 'toolbar_icon.png', 
+								      title: 'bitSurf', 
+								      message: 'Welcome! You just logged in!',
+								      priority: 0},
+								  function() { /* Error checking goes here */} 
+
+								);
+
 			chrome.storage.local.set({'bitcoin_addr': $("#bitcoin_addr").val(),
                                      'total_earned': data['total_earned']}, function () {
                                         window.location.replace("status.html");
