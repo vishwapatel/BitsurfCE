eb49710c4e04741224b7448b77c0b735038d3335
david <david_wan@rocketmail.com>
2013-09-07 21:06:14 -0400

    Fixed more issues with stupid bugs

--- background.js
+++ background.js
@@ -18,7 +18,7 @@ function onMessage(request, sender, sendResponse) {
                 chrome.storage.local.get("bitcoin_addr", function(data) {
                         var callUrl = 'http://ec2-23-22-205-148.compute-1.amazonaws.com:8000/send-payment?bitcoin_addr=' + data['bitcoin_addr'] + '&website=' + request.url;
                         console.log(callUrl);
-                        onMessage({action:'getJSON',url:callUrl}, function (data) {
+                        onMessage({action:'getJSON',url:callUrl}, null, function (data) {
                                 var total_earned = data['total_earned'];
                                 alert("YOUR TOTAL IS : "+ total_earned);
                                 chrome.storage.local.set({"total_earned": total_earned}, function(response) {});
--- home.js
+++ home.js
@@ -9,11 +9,11 @@ $(document).ready(function() {
 
     $("#login_button").click( function (e) {
     	console.log($("#bitcoin_addr").val());
-    	var callUrl = 'http://ec2-23-22-205-148.compute-1.amazonaws.com:8000/login?bitcoin_addr=' + $("#bitcoin_addr").val();
+    	var callUrl = 'http://ec2-23-22-205-148.compute-1.amazonaws.com:8000/login?bitcoin_addr=' + $("#bitcoin_addr").text("#bitcoin_addr").val();
 
         chrome.runtime.sendMessage({action:'getJSON',url:callUrl}, function(data) {
 			chrome.storage.local.set({'bitcoin_addr': $("#bitcoin_addr").val(),
-                                     'current_balance': data['current_balance']}, function () {
+                                     'total_earned': data['total_earned']}, function () {
                                         window.location.replace("status.html");
                                      });
         });
